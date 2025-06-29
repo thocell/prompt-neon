d2a82f10cf5e379037cb02061f07e7769305f4d1
thocell <6095282+thocell@users.noreply.github.com>
2025-06-29 22:33:05 +0800

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,32 +1,27 @@
-import "@styles/globals.css";
-import Nav from "@components/Nav";
-import SessionProvider from "@components/SessionProvider";
-import { auth } from "@auth";
+import './globals.css'
+import Nav from '@/components/Nav'
+import Provider from '@/components/Provider'
 
 export const metadata = {
-  title: "Prompt-X",
-  description: "Discover & Share AI Prompts",
-};
+  title: 'PromptX',
+  description: 'Discover & Share AI Prompts',
+}
 
-const RootLayout = async ({ children }) => {
-  const session = await auth();
+const RootLayout = ({ children }) => (
+  <html lang='en'>
+    <body>
+      <Provider>
+        <div className='main'>
+          <div className='gradient' />
+        </div>
 
-  return (
-    <html lang="en">
-      <body>
-        <SessionProvider>
-          <div className="main">
-            <div className="gradient" />
-          </div>
+        <main className='app'>
+          <Nav />
+          {children}
+        </main>
+      </Provider>
+    </body>
+  </html>
+)
 
-          <main className="app">
-            <Nav />
-            {children}
-          </main>
-        </SessionProvider>
-      </body>
-    </html>
-  );
-};
-
-export default RootLayout;
+export default RootLayout
