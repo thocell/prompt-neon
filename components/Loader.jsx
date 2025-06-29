7ed4337544a646435275b64e6bd59e9bd1e34dc9
thocell <6095282+thocell@users.noreply.github.com>
2025-06-29 22:06:19 +0800

    Update Loader.jsx

--- components/Loader.jsx
+++ components/Loader.jsx
@@ -1,9 +1,18 @@
-import React from "react";
-import { RotateSpinner } from "react-spinners-kit";
+import React from 'react';
 
-const Loader = () => {
+const Loader = ({ size = 'default' }) => {
+  const sizeClasses = {
+    small: 'w-4 h-4',
+    default: 'w-8 h-8',
+    large: 'w-12 h-12'
+  };
+
   return (
-      <RotateSpinner size={30} color="#f7203c" />
+    <div className="flex items-center justify-center p-4">
+      <div 
+        className={`${sizeClasses[size]} border-4 border-blue-200 border-t-blue-600 rounded-full animate-spin`}
+      />
+    </div>
   );
 };
 
